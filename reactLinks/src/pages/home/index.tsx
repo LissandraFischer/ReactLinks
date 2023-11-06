48e25fa2c69da04de2a76cbc84c9ebb2e79e418f
LissandraFischer <145466916+LissandraFischer@users.noreply.github.com>
2023-11-06 16:03:20 +0000

    links

--- reactLinks/src/pages/home/index.tsx
+++ reactLinks/src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+
 import { Social } from '../../components/Social'
 
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
@@ -6,7 +7,8 @@ export function Home(){
   return(
     <div className="flex flex-col w-full py-4 items-center justify-center">
       <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Instituto Federal Catarinense</h1>
-      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+      <span className="text-whi mb-5 mt-3">Veja meus links 👇</span>
+      
 
       <main className="flex flex-col w-11/12 max-w-xl text-center">
         <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
