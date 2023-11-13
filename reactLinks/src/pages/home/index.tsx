dd6d1ed9f336b153e1b0c8c7213771309ea24059
LissandraFischer <145466916+LissandraFischer@users.noreply.github.com>
2023-11-13 11:26:09 +0000

    atualização tailwindcss

--- reactLinks/src/pages/home/index.tsx
+++ reactLinks/src/pages/home/index.tsx
@@ -7,7 +7,7 @@ export function Home(){
   return(
     <div className="flex flex-col w-full py-4 items-center justify-center">
       <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Instituto Federal Catarinense</h1>
-      <span className="text-whi mb-5 mt-3">Veja meus links 👇</span>
+      <span className="text-white mb-5 mt-3 ">Veja meus links 👇</span>
       
 
       <main className="flex flex-col w-11/12 max-w-xl text-center">
--- reactLinks/src/pages/login/index.tsx
+++ reactLinks/src/pages/login/index.tsx
@@ -35,8 +35,8 @@ export function Login(){
   return(
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
-        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">Dev
-        <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
+        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">IF
+        <span className="bg-gradient-to-r from-green-300 to-green-500 bg-clip-text text-transparent">Links</span>
         </h1>
       </Link>
 
