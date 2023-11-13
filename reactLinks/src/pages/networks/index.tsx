dbecf783eba936e318d9b81fbe7bed510ba411fc
LissandraFischer <145466916+LissandraFischer@users.noreply.github.com>
2023-11-13 12:40:34 +0000

    atualização estrutura pagina redessociais

--- reactLinks/src/pages/networks/index.tsx
+++ reactLinks/src/pages/networks/index.tsx
@@ -1,8 +1,54 @@
+import { useState, FormEvent} from 'react'
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+
+
 
 export function Networks(){
+  const [facebook, setFacebook] = useState("")
+  const [instagram, setInstagram] = useState("")
+  const [youtube, setYoutube] = useState("")
+
+  
+
   return(
-    <div>
-      <h1>Página redes sociais</h1>
+    <div className="flex items-center flex-col min-h-screen pb-7 px-2">
+      <Header/>
+
+      <h1 className="text-white text-2xl font-medium mt-8 mb-4">Minhas redes sociais</h1>
+
+      <form className="flex flex-col max-w-xl w-full" >
+        <label className="text-white font-medium mt-2 mb-2">Link do facebook</label>
+        <Input
+          type="url"
+          placeholder="Digite a url do facebook..."
+          value={facebook}
+          onChange={ (e) => setFacebook(e.target.value) }
+        />
+
+        <label className="text-white font-medium mt-2 mb-2">Link do Instagram</label>
+        <Input
+          type="url"
+          placeholder="Digite a url do instagram..."
+          value={instagram}
+          onChange={ (e) => setInstagram(e.target.value) }
+        />
+
+        <label className="text-white font-medium mt-2 mb-2">Link do Yotube</label>
+        <Input
+          type="url"
+          placeholder="Digite a url do youtube..."
+          value={youtube}
+          onChange={ (e) => setYoutube(e.target.value) }
+        />
+
+        <button 
+        type="submit"
+        className="text-white bg-blue-600 h-9 rounded-md items-center justify-center flex mb-7 font-medium"
+        >
+          Salvar links
+        </button>
+      </form>
     </div>
   )
 }
