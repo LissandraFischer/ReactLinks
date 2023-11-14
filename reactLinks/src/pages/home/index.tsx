f4342855fc8dac548a678f4533bd6827475fd486
LissandraFischer <145466916+LissandraFischer@users.noreply.github.com>
2023-11-14 13:57:54 +0000

    atualizacao

--- reactLinks/src/pages/home/index.tsx
+++ reactLinks/src/pages/home/index.tsx
@@ -4,11 +4,11 @@ import { Social } from '../../components/Social'
 
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
 
-import {db} from '../../service/firebaseConnection'
+import { db } from '../../services/firebaseConnection'
 import{
   getDocs, 
   collection,
-  orderby, 
+  orderBy, 
   query,
   doc,
   getDoc,
@@ -31,9 +31,51 @@ interface socialLinksProp{
 
 
 export function Home(){
-  const [links, setLinks] = useState<LinkProps[]>([]);
-  const [socialLinks, setSocialLnks] = useState<socialLinksProp>()
+  const [links, setLinks] = useState<LinksProps[]>([]);
+  const [socialLinks, setSocialLinks] = useState<socialLinksProp>()
 
+  useEffect (()=>{
+    function carregarLinks(){
+      const linksRef = collection(db, "links")
+      const queryRef = query(linksRef, orderBy("created", "asc"))
+    
+      getDocs(queryRef)
+      .then((snapshot) => {
+        let lista = [] as LinksProps[];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            name: doc.data().name,
+            url: doc.data().url,
+            bg: doc.data().bg,
+            color: doc.data().color,
+
+          })
+        })
+        setLinks(lista);
+      })
+    }
+    carregarLinks();
+  },[])
+
+  useEffect(()=>{
+    function carregarSocialLinks(){
+      const docRef = doc(db, "social", "link")
+
+      getDoc(docRef)
+      .then((snapshot)=>{
+        if(snapshot.data() !== undefined){
+          setSocialLinks({
+            facebook: snapshot.data()?.facebook,
+            instagram: snapshot.data()?.instagram,
+            youtube: snapshot.data()?.youtube,
+          })
+        }
+      })
+    }
+    carregarSocialLinks();
+  },[])
 
   return(
     <div className="flex flex-col w-full py-4 items-center justify-center">
@@ -42,27 +84,34 @@ export function Home(){
       
 
       <main className="flex flex-col w-11/12 max-w-xl text-center">
-        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
-          <a>
-            <p className="text-base md:text-lg">
-              Canal no Youtube
+        {links.map((link) => (
+          <section 
+          style={{background: link.bg}}
+          key={link.id}
+          className=" mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a href={link.url} target="_blank">
+            <p className="text-base md:text-lg" style={{color: link.color}}>
+              {link.name}
             </p>
           </a>
         </section>
+        ))}
 
-        <footer className="flex justify-center gap-3 my-4">
-          <Social url="https://www.facebook.com/oficial.ifc/">
+        {socialLinks && Object.keys(socialLinks).length > 0 && (
+          <footer className="flex justify-center gap-3 my-4">
+          <Social url={socialLinks?.facebook}>
             <FaFacebook size={35} color="#FFF" />
           </Social>
 
-          <Social url="https://www.youtube.com/user/IFCatarinense">
+          <Social url={socialLinks?.youtube}>
             <FaYoutube size={35} color="#FFF" />
           </Social>
 
-          <Social url="https://www.instagram.com/ifc.oficial/">
+          <Social url={socialLinks?.instagram}>
             <FaInstagram size={35} color="#FFF" />
           </Social>
         </footer>
+        )}
 
       </main>
 
